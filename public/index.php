51021025b5004f6390ad62be4fa02c179b9d6e14
Randima Dias <randimamethminid525@gmail.com>
2022-11-08 12:53:46 +0530

    feat: implement controller classes

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+use app\controllers\SiteController;
+use app\core\Application;
+use app\controllers\AuthController;
+
+
+$app = new Application(dirname(__DIR__));
+
+$app->router->get('/',[SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+$app->router->get('/product-seller-signup', [AuthController::class, 'getProductSellerSignupPage']);
+$app->router->post('/product-seller-signup', [AuthController::class, 'registerProductSeller']);
+$app->router->get('/product-seller-signup', [AuthController::class, 'getProductSellerLoginPage']);
+$app->router->post('/product-seller-signup', [AuthController::class, 'loginProductSeller']);
+
+
+
+$app->run();
+
+
+?>
