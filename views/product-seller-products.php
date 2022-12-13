f303b8dfee0774384d07cba581ec0a715eb2b27e
Randima Dias <randimamethminid525@gmail.com>
2022-12-13 13:45:47 +0530

    feat: added product seller authentication and dashboard

---
+++ views/product-seller-products.php
@@ -0,0 +1,123 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/6fcf003f29.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Elsie&family=Raleway:wght@800&family=Roboto&display=swap" rel="stylesheet">
+
+
+    <title>Medicinal Fruits and Vegetables</title>
+</head>
+<body>
+<div class="header">
+    <div class="header-logo">
+        <img class="logo" src="/assets/images/logo.jpg" alt="logo">
+    </div>
+
+    <div class="header-title">
+        <h2>Aura</h2>
+        <h5>Community Health Net</h5>
+    </div>
+</div>
+
+<div class="title">
+    <h2 class="title-text">Medicinal Fruits & Vegetables</h2>
+</div>
+
+<table class="table">
+    <tr>
+        <th></th>
+        <th>ID Number</th>
+        <th>Product Name</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Stock</th>
+    </tr>
+    
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>1</td>
+        <td>Vali Anoda</td>
+        <td>1 kg</td>
+        <td>Rs. 150</td>
+        <td>40 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+    <tr>
+        <td><img class="products-img" src="assets/images/vali anoda.jfif" alt=""></td>
+        <td>2</td>
+        <td>Nil Veralu</td>
+        <td>100 g</td>
+        <td>Rs. 350</td>
+        <td>1 kg</td>
+    </tr>
+
+
+
+</table>
+
+</body>
+
